4eeb3ce9b17ba6c899a0e90994a40ca8aa130e7c
amdad-orangetoolz <amdad@orangetoolz.com>
2021-10-17 09:33:09 +0600

    chnaged return data method

--- src/social/User.jsx
+++ src/social/User.jsx
@@ -8,7 +8,13 @@ export class User extends Component {
     }
 
     render(){
-        const { provider, profile } = this.props;
+       // const { provider, profile } = this.props;
+        const profile = {
+            "name": "Pluralsight",
+            "number": 1,
+            "address": "India",
+            "website": "https://www.pluralsight.com/"
+        }
         return(
             <div>
             {
